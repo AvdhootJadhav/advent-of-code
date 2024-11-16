f2de3e08ea33b30c2182a2a76f0b5f609aa914c3
AvdhootJadhav <avdhootjadhav002@gmail.com>
2024-11-16 13:38:56 +0530

    comaprison logic for puzzle

--- 2019/third.ex
+++ 2019/third.ex
@@ -1,5 +1,4 @@
 defmodule Third do
-
   defmodule Point do
     defstruct x: 0, y: 0
   end
@@ -19,8 +18,6 @@ defmodule Third do
             trace_wires(x, parent)
             # send(self(), {:done, result})
           end)
-
-        IO.puts("Process started at pid : #{inspect(pid)}")
         pid
       end)
 
@@ -37,11 +34,11 @@ defmodule Third do
     IO.puts("First : #{inspect(first)}")
     IO.puts("Second : #{inspect(second)}")
 
-    for x <- first do
-      for y <- second do
-        check_intersection(x, y)
-      end
-    end
+    set = for x <- first, y <- second, do: get_distance(x, y)
+
+    res = Enum.min(Enum.filter(set, fn x -> x != nil end))
+
+    IO.puts("res : #{res}")
   end
 
   def trace_wires(directions, id) do
@@ -73,26 +70,53 @@ defmodule Third do
     send(id, {:done, data})
   end
 
-  def check_intersection(l1, l2) do
-    start1 = List.first(l1)
-    end1 = List.last(l1)
+  def get_distance([x, y], [a, b]) do
+    start1 = x
+    end1 = y
 
-    start2 = List.first(l2)
-    end2 = List.last(l2)
+    start2 = a
+    end2 = b
 
-    slope1 = if elem(end1, 0)-elem(start1, 0) == 0 do
-      false
-    else true
+    slope1 = calculate_slope(start1, end1)
+    slope2 = calculate_slope(start2, end2)
+    if slope1 != slope2 do
+      point = calculate_intersection(start1, end1, start2, end2)
+
+      case point do
+        {0, 0} ->
+          nil
+
+        {p, q} ->
+          if p >= elem(start1, 0) && p <= elem(end1, 0) && q >= elem(start1, 1) &&
+               q <= elem(end1, 1) do
+            p+q
+          end
+
+        _ ->
+          nil
+      end
     end
+  end
 
-    slope2 = if elem(end2, 0)-elem(start2, 0) == 0 do
-      false
-    else true
+  def calculate_intersection({x1, y1}, {x2, y2}, {x3, y3}, {x4, y4}) do
+    if y1 == y2 do
+      if x3 >= min(x1, x2) && x3 <= max(x1, x2) do
+        {x3, y1}
+      end
+    else
+      if x1 == x2 do
+        if x1 >= min(x3, x4) && x1 <= max(x3, x4) do
+          {x1, y4}
+        end
+      end
     end
+  end
 
-    if slope1 && slope2 do
-      IO.puts("Line 1 : #{inspect(start1)} #{inspect(end1)}")
-      IO.puts("Line 2 : #{inspect(start2)} #{inspect(end2)}")
+  def calculate_slope({x1, y1}, {x2, y2}) do
+    case x2 - x1 do
+      0.0 -> false
+      0 -> false
+      dx -> abs((y2 - y1) / dx)
     end
   end
 
