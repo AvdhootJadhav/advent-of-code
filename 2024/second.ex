9a0879e0ab3b34c5fdf7962ce6fd7f2d58e1ffb8
AvdhootJadhav <avdhootjadhav002@gmail.com>
2024-12-07 12:15:11 +0530

    day2 part-1

---
+++ 2024/second.ex
@@ -0,0 +1,42 @@
+defmodule Second do
+  def main() do
+    input =
+      Enum.map(read(), fn x ->
+        Enum.map(String.split(to_string(x), " "), fn y -> get_integer(y) end)
+      end)
+      |> Enum.filter(fn x -> Enum.sort(x) == x || Enum.reverse(Enum.sort(x)) == x end)
+
+    {_, c} = Enum.reduce(input, {0,0}, fn x,{last,count}->
+      {l, c} = Enum.reduce_while(tl(x), {hd(x), 0}, fn y,{last, count} ->
+        if abs(y-last) >= 1 && abs(y-last) <= 3 do
+          count = count + 1
+          {:cont, {y,count}}
+        else {:halt, {last, count}}
+        end
+      end)
+      if c === length(x)-1 do
+        count = count+1
+        last = l
+        {last, count}
+      else {last, count}
+      end
+    end)
+
+    IO.puts("count : #{c}")
+  end
+
+  def read() do
+    stream = File.stream!("../demo.txt")
+    inputList = Enum.to_list(Enum.map(stream, fn x -> String.trim(x) end))
+    inputList
+  end
+
+  def get_integer(input) do
+    case Integer.parse(input) do
+      {number, ""} -> number
+      _ -> nil
+    end
+  end
+end
+
+Second.main()
