c91cb2afa184c48940c31a6165b01f385c19920b
AvdhootJadhav <avdhootjadhav002@gmail.com>
2024-12-14 12:38:43 +0530

    part-2 code

--- 2024/third.ex
+++ 2024/third.ex
@@ -3,9 +3,30 @@ defmodule Third do
     input = read()
 
     values = Enum.map(input, fn x->
-      filtered = Regex.scan(~r/mul[(][0-9]{1,3},[0-9]{1,3}[)]/, x)
+      filtered = Regex.scan(~r/mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)/, x)
       Enum.map(filtered, fn [y] -> y end)
     end)
+
+    values = Enum.map(values, fn x ->
+      {_, data} = Enum.reduce(x, {true, []}, fn x,{take,data}->
+        case x do
+          "do()" ->
+            {true, data}
+          "don't()" ->
+            {false, data}
+          _ ->
+            if take do
+              data = [x|data]
+              {take, data}
+            else
+              {take, data}
+            end
+        end
+      end)
+      data
+    end)
+
+    formatted_input = values
     |> Enum.map(fn x->
       Enum.map(x, fn y->
         String.replace(y, "mul(", "")
@@ -15,10 +36,10 @@ defmodule Third do
       end)
     end)
 
-    result = Enum.reduce(values, 0, fn x,sum->
-      sum = Enum.reduce(x, sum, fn [a,b],sum->
-        sum = sum + a*b
-        sum
+    result = Enum.reduce(formatted_input, 0, fn x,sum->
+      sum = Enum.reduce(x, sum, fn [a,b],c->
+        c = c + a*b
+        c
       end)
       sum
     end)
